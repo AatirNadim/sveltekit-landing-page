c2d25a7d44243860bdd6177b7833a3b5434ce9fc
AatirNadim <aatir.nadim@gmail.com>
2024-03-07 19:02:23 +0530

    navbar done for now

--- src/lib/components/custom/navbar/navbar.svelte
+++ src/lib/components/custom/navbar/navbar.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
   import Banner from "@/assets/SVGs/banner.svelte";
   import Logo from "@/assets/SVGs/logo.svelte";
-  import Button from "../../ui/button/button.svelte";
+  import Button from "@/components/ui/button/button.svelte"
   import NavBtns from "./navBtns.svelte";
+  import Person from "@/assets/PNGs/person.jpeg"
   import Plus from "@/assets/SVGs/plus.svelte";
+  import ArrowDown from "@/assets/SVGs/navbar/arrowDown.svelte";
 </script>
 
 <navbar class="w-full flex items-center p-4 justify-between min-h-[73px]">
@@ -16,10 +18,19 @@
   >
     <section class="w-[461px] flex items-center justify-between">
       <NavBtns />
-      <Button variant="outline" class="flex items-center gap-2">
+      <Button
+        variant="outline"
+        class="flex items-center gap-2 border-b-4 border-black w-[102px]"
+      >
         <Plus />
         <span>Post</span>
       </Button>
+    </section>
+    <section class="flex w-[248px] items-center justify-evenly" >
+      <img src={Person} alt="person" class="rounded-full w-[48px] h-[48px]" />
+      <span class="text-[#4D4D4D] text-xl" >Krishna Kiran</span>
+      
+      <ArrowDown />
     </section>
   </section>
 </navbar>

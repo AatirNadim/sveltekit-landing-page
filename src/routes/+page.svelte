f9d5217f9acaa6afd7ce7a81e392234ff69ca46e
AatirNadim <aatir.nadim@gmail.com>
2024-03-07 16:09:10 +0530

    base setup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
+<script lang="ts">
+  import Button from "@/components/ui/button/button.svelte";
+  import { Calendar } from "$lib/components/ui/calendar";
+  import { today, getLocalTimeZone } from "@internationalized/date";
+ 
+  let value = today(getLocalTimeZone());
+</script>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
+<Button>Click me</Button>
+<Calendar bind:value class="rounded-md border" />

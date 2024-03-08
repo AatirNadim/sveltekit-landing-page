e818a788c35a7cb5f35d36b39c188a7924b5e03d
AatirNadim <aatir.nadim@gmail.com>
2024-03-08 20:19:27 +0530

    ui additions

--- src/lib/components/custom/info/positionInfo.svelte
+++ src/lib/components/custom/info/positionInfo.svelte
@@ -2,6 +2,9 @@
   import LogoRound from "@/assets/SVGs/logoRound.svelte";
   import Link from "@/assets/SVGs/positionInfo/link.svelte";
   import ThreeDots from "@/assets/SVGs/positionInfo/threeDots.svelte";
+  import Button from "@/components/ui/button/button.svelte";
+  import * as Dropdown from "@/components/ui/dropdown-menu";
+  import { Root } from "postcss";
 </script>
 
 <section class="flex flex-col items-end justify-between h-full">
@@ -10,11 +13,35 @@
     <span class="font-bold text-xl">Duggup</span>
     <span class="text-[#7A9299] font-medium text-xs">Co-Founder and CEO</span>
   </section>
-  <section class="flex items-center justify-between gap-4">
-    <span class="flex items-center gap-1">
+  <section class="flex items-center justify-between gap-2">
+    <Button
+      class="flex items-center gap-1 px-1 py-0.5"
+      variant="ghost"
+      href="https://drive.google.com/file/d/1mlF2Ed578KnMliXdw7wbHRqJWwFmcLMS/view?usp=sharing"
+      target="_blank"
+    >
       My website
       <Link />
-    </span>
-    <ThreeDots />
+    </Button>
+    <Dropdown.Root>
+      <Dropdown.Trigger>
+        <Button variant="ghost" class="rounded-full p-1 px-2">
+          <ThreeDots />
+        </Button>
+      </Dropdown.Trigger>
+      <Dropdown.Content>
+        <Dropdown.Label>Section 1</Dropdown.Label>
+        <Dropdown.Group>
+          <Dropdown.Item>Option 1</Dropdown.Item>
+          <Dropdown.Item>Option 2</Dropdown.Item>
+        </Dropdown.Group>
+        <Dropdown.Separator />
+        <Dropdown.Label>Section 2</Dropdown.Label>
+        <Dropdown.Group>
+          <Dropdown.Item>Option 3</Dropdown.Item>
+          <Dropdown.Item>Option 4</Dropdown.Item>
+        </Dropdown.Group>
+      </Dropdown.Content>
+    </Dropdown.Root>
   </section>
 </section>
--- src/lib/components/custom/navbar/navbar.svelte
+++ src/lib/components/custom/navbar/navbar.svelte
@@ -1,40 +1,60 @@
 <script lang="ts">
   import Banner from "@/assets/SVGs/banner.svelte";
   import Logo from "@/assets/SVGs/logo.svelte";
-  import Button from "@/components/ui/button/button.svelte"
+  import Button from "@/components/ui/button/button.svelte";
   import NavBtns from "./navBtns.svelte";
-  import Person from "@/assets/PNGs/person.jpeg"
+  import Person from "@/assets/PNGs/person.jpeg";
   import Plus from "@/assets/SVGs/plus.svelte";
   import ArrowDown from "@/assets/SVGs/navbar/arrowDown.svelte";
   import Dotted from "@/assets/SVGs/navbar/dotted.svelte";
+  import * as Dropdown from "@/components/ui/dropdown-menu"
 </script>
 
-<div class="flex flex-col w-full mb-12" >
-<navbar class="w-full flex items-center p-4 justify-between min-h-[73px]">
-  <section class="flex items-center justify-center gap-2">
-    <Logo />
-    <Banner height="" width="" />
-  </section>
-  <section
-    class="flex flex-col md:flex-row items-center justify-between w-[749px] h-[52px] gap-[40px]"
-  >
-    <section class="w-[461px] flex items-center justify-between">
-      <NavBtns />
-      <Button
-        variant="outline"
-        class="flex items-center gap-2 border-b-4 border-black w-[102px]"
-      >
-        <Plus />
-        <span>Post</span>
-      </Button>
+<div class="flex flex-col w-full mb-12">
+  <navbar class="w-full flex items-center p-4 justify-between min-h-[73px]">
+    <section class="flex items-center justify-center gap-2">
+      <Logo />
+      <Banner height="" width="" />
     </section>
-    <section class="flex w-[248px] items-center justify-evenly" >
-      <img src={Person} alt="person" class="rounded-full w-[48px] h-[48px]" />
-      <span class="text-[#4D4D4D] text-xl" >Krishna Kiran</span>
-      
-      <ArrowDown />
+    <section
+      class="flex flex-col md:flex-row items-center justify-between w-[749px] h-[52px] gap-[40px]"
+    >
+      <section class="w-[461px] flex items-center justify-between">
+        <NavBtns />
+        <Button
+          variant="outline"
+          class="flex items-center gap-2 border-b-4 border-black w-[102px]"
+        >
+          <Plus />
+          <span>Post</span>
+        </Button>
+      </section>
+      <section class="flex w-[248px] items-center justify-evenly">
+        <img src={Person} alt="person" class="rounded-full w-[48px] h-[48px]" />
+        <Dropdown.Root  >
+          <Dropdown.Trigger  >
+            <Button class="gap-1 justify-between " variant="ghost">
+              <span class="text-[#4D4D4D] text-xl">Krishna Kiran</span>
+              <ArrowDown />
+            </Button>
+          </Dropdown.Trigger>
+          <Dropdown.Content  >
+            <Dropdown.Label >Section 1</Dropdown.Label>
+            <Dropdown.Group >
+              <Dropdown.Item >Option 1</Dropdown.Item>
+              <Dropdown.Item >Option 2</Dropdown.Item>
+            </Dropdown.Group>
+            <Dropdown.Separator />
+            <Dropdown.Label >Section 2</Dropdown.Label>
+            <Dropdown.Group >
+              <Dropdown.Item >Option 3</Dropdown.Item>
+              <Dropdown.Item >Option 4</Dropdown.Item>
+            </Dropdown.Group>
+          </Dropdown.Content>
+        </Dropdown.Root>
+        
+      </section>
     </section>
-  </section>
-</navbar>
-<Dotted />
+  </navbar>
+  <Dotted />
 </div>
